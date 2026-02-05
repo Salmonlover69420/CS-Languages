bde8f436362730d9807fb85710530e236a72731a
Chimpanzee <83028073+Salmonlover69420@users.noreply.github.com>
2026-02-05 19:25:03 +0200

    Integer Math

---
+++ C/3intMultiplication.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main () {
+    int A, B, C;
+    float op;
+
+    printf("Enter 3 integers to compute (A*B)/2: ");
+    scanf("%i%i%i", &A, &B, &C);
+    
+    op = (float)(A*B)/C;
+
+    if (C != 0)
+    printf("The answer is %.2f", op);
+    else {
+        printf("Invalid input for C.");
+
+        return 1;
+    }
+
+    return 0;
+}
